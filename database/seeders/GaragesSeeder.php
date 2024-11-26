68391bfcd3de9f6ccb15717b677e1a8850b71474
engineertuhin <tuhin1191@gmail.com>
2024-11-26 10:27:37 +0600

    add Garage seeder and migration

---
+++ database/seeders/GaragesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GaragesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $zones = [
+            ['name' => 'CNG'],
+            ['name' => 'Bus'],
+            ['name' => 'Truck'],
+            ['name' => 'Motors '],
+            ['name' => 'Private Care'],
+        ];
+
+        DB::table('zones')->insert($zones);
+    }
+}
